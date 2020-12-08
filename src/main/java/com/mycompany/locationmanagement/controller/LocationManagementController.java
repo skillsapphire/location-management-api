a3f0fc102f89d921bc5593730789ba66150670ff
skillsapphire <skillsapphire@gmail.com>
2020-12-08 20:13:35 +0530

    added location entity and DB config

--- src/main/java/com/mycompany/locationmanagement/controller/LocationManagementController.java
+++ src/main/java/com/mycompany/locationmanagement/controller/LocationManagementController.java
@@ -2,18 +2,20 @@ package com.mycompany.locationmanagement.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+
 @RestController
 @RequestMapping("/lma/v1")
 public class LocationManagementController {
 
     @GetMapping("/locations")
     public void getAllLocations(){
-
+        System.out.println("locations");
     }
 
     @GetMapping("/locations/{id}")
     public void getLocationDetail(){
-
+        System.out.println("locations/id");
     }
 
     @PostMapping("/locations")
---
+++ src/main/java/com/mycompany/locationmanagement/entity/Location.java
@@ -0,0 +1,49 @@
+package com.mycompany.locationmanagement.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String type;
+    private String lat;
+    private String lng;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+}
