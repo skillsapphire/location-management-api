ec77855c7b6d914a8e54ec0134c0915e27ee7edb
skillsapphire <skillsapphire@gmail.com>
2020-12-08 14:37:11 +0530

    initial project setup

---
+++ src/main/java/com/mycompany/locationmanagement/LocationManagementApp.java
@@ -0,0 +1,13 @@
+package com.mycompany.locationmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationManagementApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationManagementApp.class, args);
+	}
+
+}
